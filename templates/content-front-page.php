ce809638f0a08b3d8ac5c0c7c8c335de28be9c26
tapps <t@tapps.email>
2016-06-24 02:34:20 -0500

    start of ACF loop
    not working right though

--- templates/content-front-page.php
+++ templates/content-front-page.php
@@ -2,15 +2,23 @@
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
 
 <?php
-$additional_page_content_array = get_field( 'additional_page_content' );
+$additional_page_content_array = get_sub_field( 'additional_page_content' );
 
-/*// check if the repeater field has rows of data
+print_r($additional_page_content_array);
+
+// check if the repeater field has rows of data
 if( have_rows('additional_page_content') ):
 
 	// loop through the rows of data
 	while ( have_rows('additional_page_content') ) : the_row();
 
-		the_sub_field('page_content_column');
+		echo '<div class="column"><hr>';
+
+		$column_array = get_field( 'page_content_row' );
+		print_r($column_array);
+		echo $column_array;
+
+		echo '</div>';
 
 	endwhile;
 
@@ -18,6 +26,6 @@ else :
 
 	// no rows found
 
-endif;*/
+endif;
 
 ?>
